5b1fc064fba3a008ea2dd29d141a45b8ee37b7ca
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-21 09:15:54 -0700

    finish carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,48 @@
 class Carousel {
+  constructor(elementIn) {
+    this.element = elementIn;
+    this.currentIndex = 0;
+    this.slides = Array.from(this.element.querySelectorAll('img')).map(
+      slide => new Slide(slide)
+    );
+    this.leftButton = this.element.querySelector('.left-button');
+    this.rightButton = this.element.querySelector('.right-button');
+    this.leftButton.addEventListener('click', () => this.prevSlide());
+    this.rightButton.addEventListener('click', () => this.nextSlide());
+  }
 
+  nextSlide() {
+    this.slides[this.currentIndex].deselect();
+    this.currentIndex >= this.slides.length - 1
+      ? (this.currentIndex = 0)
+      : this.currentIndex++;
+    this.slides[this.currentIndex].select();
+  }
+
+  prevSlide() {
+    this.slides[this.currentIndex].deselect();
+    this.currentIndex <= 0
+      ? (this.currentIndex = this.slides.length - 1)
+      : this.currentIndex--;
+    this.slides[this.currentIndex].select();
+  }
 }
 
-let carousel = document.querySelector();
+class Slide {
+  constructor(elementIn) {
+    this.element = elementIn;
+  }
+
+  select() {
+    this.element.classList.add('current-slide');
+  }
+
+  deselect() {
+    this.element.classList.remove('current-slide');
+  }
+}
+
+new Carousel(document.querySelector('.carousel'));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
